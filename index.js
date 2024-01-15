9b14c424f7272cfce67d46c03ac4733e54415229
VitaliiKovalenko88 <75671142+VitaliiKovalenko88@users.noreply.github.com>
2024-01-15 23:33:14 +0100

    delete coment

--- index.js
+++ index.js
@@ -10,30 +10,24 @@ program.parse();
 import { addContact, getContactById, listContacts, removeContact } from "./contacts.js";
 
 const options = program.opts();
-
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
       const contacts = await listContacts();
       console.table(contacts);
       break;
 
     case "get":
-      // ... id
       const contactById = await getContactById(id);
       console.log(contactById);
       break;
 
     case "add":
-      // ... name email phone
       const newContact = await addContact(name, email, phone)
       console.log(newContact)
       break;
 
     case "remove":
-      // ... id
       const remoteContact = await removeContact(id);
       console.log(remoteContact);
       break;
