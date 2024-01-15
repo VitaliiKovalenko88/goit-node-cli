c12ba271484918e4668e306e4c919482c2ca3279
VitaliiKovalenko88 <75671142+VitaliiKovalenko88@users.noreply.github.com>
2024-01-15 19:14:51 +0100

    add terminal.js in gitignor

--- contacts.js
+++ contacts.js
@@ -2,11 +2,12 @@ import fs from "fs/promises";
 import path from "path";
 
 const contactsPath = path.resolve('./db/contacts.json');
+const getContacts = await fs.readFile(contactsPath)
 
 export async function listContacts() {
   // ...твій код. Повертає масив контактів.
   try {
-    const getContacts = await fs.readFile(contactsPath);
+
     const contacts = JSON.parse(getContacts);
     return contacts;
 
@@ -17,17 +18,27 @@ export async function listContacts() {
 
 export async function getContactById(contactId) {
   // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-  const getContacts = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(getContacts);
-  const contactById = contacts.find(contact => contact.id === contactId)
+  try {
+    ;
+    const contacts = JSON.parse(getContacts);
+    const contactById = contacts.find(contact => contact.id === contactId)
 
-  return contactById ? contactById : null;
+    return contactById ? contactById : null;
+  } catch (error) {
+    console.log(error);
+  }
+
 }
-getContactById('AeHIrLTr6JkxGE6SN-0Rw')
+
 async function removeContact(contactId) {
   // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
+  try {
 
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+removeContact('05olLMgyVQdWRwgKfg5J43456');
 async function addContact(name, email, phone) {
   // ...твій код. Повертає об'єкт доданого контакту (з id).
 }
